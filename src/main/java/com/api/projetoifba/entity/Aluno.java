ce3a308c3ae51e91fbe47845cd7b1fd5b8b391fb
Gerdany Junior <Asa_lindo@hotmail.com>
2023-02-26 14:27:59 -0300

    Criação da entidade endereço

--- src/main/java/com/api/projetoifba/entity/Aluno.java
+++ src/main/java/com/api/projetoifba/entity/Aluno.java
@@ -1,19 +1,112 @@
 package com.api.projetoifba.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "alunos")
 public class Aluno{
-    @Column(nullable = false,length = 50)
+
+    @Column(nullable = false,length = 70)
     private String nome;
+
+    @Id
     @Column(nullable = false,length = 12)
     private String matricula;
+
     @Column(nullable = false,length = 11)
     private String cpf;
-    @Column(nullable = false,length = 30)
+
+    @Column(nullable = false,length = 50)
     private String email;
-    private Endereço endereco;
+
+    @Column(nullable = false,length = 30)
+    private String curso;
+
+    @Column(nullable = false,length = 1)
+    private Character sexo; 
+
+    @OneToMany
+    private List<Endereço> enderecos = new ArrayList<>();
+
+    public Aluno(){}
+
+    public Aluno(String nome, String matricula, String cpf, String email, String curso, Character sexo,
+            Endereço endereço) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cpf = cpf;
+        this.email = email;
+        this.curso = curso;
+        this.sexo = sexo;
+        enderecos.add(endereço);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public List<Endereço> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereço> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno [nome=" + nome + ", matricula=" + matricula + ", cpf=" + cpf + ", email=" + email + ", curso="
+                + curso + ", sexo=" + sexo + ", enderecos=" + enderecos + "]";
+    }
 }
---
+++ src/main/java/com/api/projetoifba/entity/Endereço.java
@@ -0,0 +1,80 @@
+package com.api.projetoifba.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "enderecos")
+public class Endereço {
+
+    @Id
+    private UUID id;
+    @Column(nullable = false,length = 20)
+    private String rua;
+    @Column(nullable = false,length = 5)
+    private Integer numero;
+    @Column(nullable = false,length = 9)
+    private String cep;
+    @Column(nullable = true,length = 30)
+    private String complemento;
+
+    public Endereço(){}
+
+    public Endereço(UUID id, String rua, Integer numero, String cep, String complemento) {
+        this.id = id;
+        this.rua = rua;
+        this.numero = numero;
+        this.cep = cep;
+        this.complemento = complemento;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereço [id=" + id + ", rua=" + rua + ", numero=" + numero + ", cep=" + cep + ", complemento="
+                + complemento + "]";
+    }
+}
