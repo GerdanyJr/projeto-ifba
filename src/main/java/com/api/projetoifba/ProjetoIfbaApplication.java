663c6473bb38ae3c1d4758dee3faba3a9911c040
Gerdany Junior <Asa_lindo@hotmail.com>
2023-02-26 13:58:46 -0300

    Criação do projeto

---
+++ src/main/java/com/api/projetoifba/ProjetoIfbaApplication.java
@@ -0,0 +1,13 @@
+package com.api.projetoifba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIfbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIfbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/projetoifba/ProjetoIfbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.projetoifba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIfbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
